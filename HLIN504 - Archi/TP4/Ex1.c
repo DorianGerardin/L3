1610459b9f30752594c56d0960605a944cc270df
DorianGerardin <gryfadon@gmail.com>
2020-10-16 13:00:00 +0200

    tp4 archi en cours

---
+++ HLIN504 - Archi/TP4/Ex1.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+
+	fork();
+	printf("un nouveau descendant %d de parent %d !\n",getpid(), getppid());
+	return EXIT_SUCCESS;
+}

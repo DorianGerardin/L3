b83738b92bea7ff6d86819365454ef9e6312367e
Dorian Gerardin <43269595+DorianGerardin@users.noreply.github.com>
2021-02-01 08:39:49 +0100

    Delete HLIN508 - Algo directory

--- HLIN508 - Algo/TP4/TP4.h
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <iostream>
-
-using namespace std;
-
-int* RemplirTableau(int&, int, char**);
-void InitialiserTableau(int, int[]);
-void CopierTableau(int, int[], int[]);
-void AfficherTableau(int, int[]);
